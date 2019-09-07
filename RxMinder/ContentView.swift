82dc34a99b12a45566f27426bfbddf1689e7e3ef
nrubin29 <nrubin29@gmail.com>
2019-09-06 23:49:39 -0400

    Initial Commit

---
+++ RxMinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RxMinder
+//
+//  Created by Noah Rubin on 9/6/19.
+//  Copyright © 2019 Noah Rubin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
